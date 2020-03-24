4aab8ae9e79c3791a6ab8b8911b6fe34a87b5ad3
Eloy Barbosa <44401096+eloybarbosa@users.noreply.github.com>
2020-03-23 22:09:48 -0300

    Add files via upload
    Implentação do detalhamento das cervejarias

---
+++ src/components/Contato.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+        <h1>Finds</h1>
+            <div v-for="(find, index) in finds">
+            <input v-model="find.value">
+            <button @click="deleteFind(index)">
+            delete
+            </button>
+            </div>
+        
+        <button @click="addFind">
+            New Find
+        </button>
+        
+        <pre>{{ $data }}</pre>
+    </div>
+</template>
+
+<script>
+export default {
+    new Vue({
+    el: '#app',
+    data: {
+        finds: [] 
+    },
+    methods: {
+        addFind: function () {
+        this.finds.push({ value: 'def' });
+        },
+        deleteFind: function (index) {
+        console.log(index);
+        console.log(this.finds);
+        this.finds.splice(index, 1);
+        }
+    }
+    }),
+}
+</script>
---
+++ src/components/EditarCards.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="cerveja-edid">
+      <h1> Editar Cervejaria</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditarCerveja',
+}
+</script>
